0d93a914f86eaeb09d6781068ad89c04f9ce9e21
Michael594tech <michaeltonga30@gmail.com>
2023-08-22 08:32:28 -0700

    updated

--- __printf.c
+++ __printf.c
@@ -3,14 +3,14 @@
 void print_buffer(char output_buffer[], int *output_index);
 
 /**
- * custom_printf - Custom Printf function
+ * cust_printf - Cust Printf function
  * @format_string: Format string.
  * Return: Number of printed characters.
  */
-int custom_printf(const char *format_string, ...)
+int cust_printf(const char *format_string, ...)
 {
 	int i, printed_count = 0, total_printed_chars = 0;
-int custom_flags, cust_width, custom_precision, custom_size, output_index = 0;
+int cust_flags, cust_width, cust_precision, cust_size, output_index = 0;
 	va_list args_list;
 	char output_buffer[BUFF_SIZE];
 
@@ -31,13 +31,13 @@ int custom_flags, cust_width, custom_precision, custom_size, output_index = 0;
 		else
 		{
 			print_buffer(output_buffer, &output_index);
-			custom_flags = get_custom_flags(format_string, &i);
-			custom_width = get_custom_width(format_string, &i, args_list);
-			custom_precision = get_custom_precision(format_string, &i, args_list);
-			custom_size = get_custom_size(format_string, &i);
+			cust_flags = get_cust_flags(format_string, &i);
+			cust_width = get_cust_width(format_string, &i, args_list);
+			cust_precision = get_cust_precision(format_string, &i, args_list);
+			cust_size = get_cust_size(format_string, &i);
 			++i;
 			printed_count = handle_print(format_string, &i, args_list, output_buffer,
-					custom_flags, custom_width, custom_precision, custom_size);
+					cust_flags, cust_width, cust_precision, cust_size);
 			if (printed_count == -1)
 				return (-1);
 			total_printed_chars += printed_count;
